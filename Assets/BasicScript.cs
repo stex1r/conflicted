dfe44fd89e6d84ddb605e8b741e45ee35813782a
Ксения Попко <youryouro@icloud.com>
2022-08-10 21:59:19 +0300

    added basic script functionality

--- Assets/BasicScript.cs
+++ Assets/BasicScript.cs
@@ -7,7 +7,7 @@ public class BasicScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("awoken basic script");
     }
 
     // Update is called once per frame
