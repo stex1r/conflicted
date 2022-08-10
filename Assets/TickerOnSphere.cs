93e2d6d1b346c2edccfbce2b4f29c381c3563f13
Ксения Попко <youryouro@icloud.com>
2022-08-10 22:29:35 +0300

    added ticker disable

--- Assets/TickerOnSphere.cs
+++ Assets/TickerOnSphere.cs
@@ -6,11 +6,21 @@ public class TickerOnSphere : MonoBehaviour
     void Start()
     {
         InvokeRepeating("timerSet", 1.0f,1.0f); // line from main (fixed)
+        Invoke("disableTicker", 10.0f); // f-2 line
         Debug.Log("awoken ticking script for sphere"); // line from dependent (already has new logic)
     }
 
     public void timerSet()
     {
         Debug.Log($"tick: {DateTime.Now}");
+    }
+
+    /*
+     * f-2 method
+     */
+    public void disableTicker()
+    {
+        CancelInvoke("timerSet");
+        Debug.Log($"timer killed at: {DateTime.Now}");
     }
 }
