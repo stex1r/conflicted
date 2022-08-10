ff459b7e8c943b86826a224b29ccdde2de2bd27a
Ксения Попко <youryouro@icloud.com>
2022-08-10 22:07:21 +0300

    cleaned up for new dependent feature

--- Assets/BasicScript.cs
+++ Assets/BasicScript.cs
@@ -3,21 +3,14 @@ using UnityEngine;
 
 public class BasicScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("awoken basic script");
-        InvokeRepeating("timerSet", 0.1f,3.0f);
+        InvokeRepeating("timerSet", 1.0f,1.0f);
     }
 
     public void timerSet()
     {
         Debug.Log($"tick: {DateTime.Now}");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
