744c63db337539199f13fb1d56cdedb15cc7a611
Ксения Попко <youryouro@icloud.com>
2022-08-10 22:35:13 +0300

    made ticker lazy

--- Assets/TickerOnSphere.cs
+++ Assets/TickerOnSphere.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 
 public class TickerOnSphere : MonoBehaviour
 {
+    // f-3 line added
+    private bool first = true;
     void Start()
     {
         InvokeRepeating("timerSet", 1.0f,1.0f); // line from main (fixed)
         Invoke("disableTicker", 10.0f); // f-2 line
-        Debug.Log("awoken ticking script for sphere"); // line from dependent (already has new logic)
     }
 
     public void timerSet()
     {
-        Debug.Log($"tick: {DateTime.Now}");
+        // updated on f-3
+        Debug.Log($"tick-tock: {DateTime.Now}"); // this line on f-3 is modified
     }
 
     /*
@@ -20,6 +22,16 @@ public class TickerOnSphere : MonoBehaviour
      */
     public void disableTicker()
     {
+        /*
+         * f-3 part
+         */
+        if (first)
+        {
+            first = !first;
+            Debug.Log($"what should I do?...let's wait more ({3.0f} seconds)");
+            Invoke("disableTicker", 3.0f); // f-3 line
+            return;
+        }
         CancelInvoke("timerSet");
         Debug.Log($"timer killed at: {DateTime.Now}");
     }
