a911278a27b45ffb0705daadac24cc084139f2f5
Ксения Попко <youryouro@icloud.com>
2022-08-10 22:36:34 +0300

    - [x] fixed codestyle conventions - [x] made first f-3 replay

--- Assets/TickerOnSphere.cs
+++ Assets/TickerOnSphere.cs
@@ -7,11 +7,11 @@ public class TickerOnSphere : MonoBehaviour
     private bool first = true;
     void Start()
     {
-        InvokeRepeating("timerSet", 1.0f,1.0f); // line from main (fixed)
-        Invoke("disableTicker", 10.0f); // f-2 line
+        InvokeRepeating("TimerSet", 1.0f,1.0f); // line from main (fixed)
+        Invoke("DisableTicker", 10.0f); // f-2 line
     }
 
-    public void timerSet()
+    public void TimerSet()
     {
         // updated on f-3
         Debug.Log($"tick-tock: {DateTime.Now}"); // this line on f-3 is modified
@@ -20,7 +20,7 @@ public class TickerOnSphere : MonoBehaviour
     /*
      * f-2 method. let's pretend it was changed during merge here ...
      */
-    public void disableTicker()
+    public void DisableTicker()
     {
         /*
          * f-3 part
@@ -29,10 +29,10 @@ public class TickerOnSphere : MonoBehaviour
         {
             first = !first;
             Debug.Log($"what should I do?...let's wait more ({3.0f} seconds)");
-            Invoke("disableTicker", 3.0f); // f-3 line
+            Invoke("DisableTicker", 3.0f); // f-3 line
             return;
         }
-        CancelInvoke("timerSet");
+        CancelInvoke("TimerSet");
         /* ...
             ...
                 ...
